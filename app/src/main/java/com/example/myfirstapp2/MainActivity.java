c215b421671f664eee1040abe6db18c009a04ed1
SeanLogsdon <seanlogsdon14@gmail.com>
2020-09-23 23:26:30 -0500

    Push

--- app/src/main/java/com/example/myfirstapp2/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp2/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
     public void sendMessage(View view) {
         // Do something in response to button
